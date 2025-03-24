6d513c54fe71f30582eeb36ffb58362e632fbe64
Shubham Mishra <m.shubham0723@gmail.com>
2025-03-24 11:12:46 +0530

    added backend

---
+++ routes/authRoutes.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if ( !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) return res.status(400).json({ message: "Email already in use" });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ name, email, password: hashedPassword });
+
+  res.status(201).json({ message: "User registered successfully" });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(400).json({ message: "User does not exists" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: "Invalid email or password" });
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.json({
+    token,
+    user: { id: user._id, name: user.name, email: user.email, role: user.role },
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+connectDB();
+
+// Import Routes
+const authRoutes = require("./routes/authRoutes");
+// /const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+
+// Use Routes
+app.use("/api/auth", authRoutes);  // Handles login & signup
+//app.use("/api/users", userRoutes); // Handles user profile & other actions
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
